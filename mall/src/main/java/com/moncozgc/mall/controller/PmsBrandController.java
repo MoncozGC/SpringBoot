5dd66dca768f0bdc9ed00a14abfe84df6349a862
MoncozGC <jadepengc@163.com>
2022-11-22 10:27:01 +0800

    feat: mall - 品牌管理Controller和Service实现类

---
+++ mall/src/main/java/com/moncozgc/mall/controller/PmsBrandController.java
@@ -0,0 +1,94 @@
+package com.moncozgc.mall.controller;
+
+import com.moncozgc.mall.common.api.CommonPage;
+import com.moncozgc.mall.common.api.CommonResult;
+import com.moncozgc.mall.mbg.model.PmsBrand;
+import com.moncozgc.mall.service.PmsBrandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 品牌管理Controller
+ * Created by MoncozGC on 2022/11/21
+ */
+@Controller
+@RequestMapping("/brand")
+public class PmsBrandController {
+    @Autowired
+    private PmsBrandService brandService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
+
+    /**
+     * 查看所有商品信息
+     */
+    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<PmsBrand>> getBrandList() {
+        return CommonResult.success(brandService.listAllBrand());
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult createBrand(@RequestBody PmsBrand brand) {
+        CommonResult commonResult;
+        int count = brandService.createBrand(brand);
+
+        if (count == 1) {
+            commonResult = CommonResult.success(brandService);
+            LOGGER.debug("createBrand SUCCESS:{}", brand);
+        } else {
+            commonResult = CommonResult.failed("创建失败");
+            LOGGER.debug("createBrand FAILED:{}", brand);
+        }
+        return commonResult;
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result) {
+        CommonResult commonResult;
+        int count = brandService.updateBrand(id, pmsBrandDto);
+        if (count == 1) {
+            commonResult = CommonResult.success(pmsBrandDto);
+            LOGGER.debug("updateBrand success:{}", pmsBrandDto);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("updateBrand failed:{}", pmsBrandDto);
+        }
+        return commonResult;
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult deleteBrand(@PathVariable("id") Long id) {
+        int count = brandService.deleteBrand(id);
+        if (count == 1) {
+            LOGGER.debug("deleteBrand success :id={}", id);
+            return CommonResult.success(null);
+        } else {
+            LOGGER.debug("deleteBrand failed :id={}", id);
+            return CommonResult.failed("操作失败");
+        }
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                                        @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
+        List<PmsBrand> brandList = brandService.listBrand(pageNum, pageSize);
+        return CommonResult.success(CommonPage.restPage(brandList));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<PmsBrand> brand(@PathVariable("id") Long id) {
+        return CommonResult.success(brandService.getBrand(id));
+    }
+}
