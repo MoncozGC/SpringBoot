95fb52536c487a3fea43a0dda862f903c8c5acfa
MoncozGC <jadepengc@163.com>
2022-12-05 20:39:04 +0800

    feat: mall - 工具类, 提供字符串和时间的工具类

---
+++ mall/src/main/java/com/moncozgc/mall/common/utils/StringUtils.java
@@ -0,0 +1,177 @@
+package com.moncozgc.mall.common.utils;
+
+
+import java.text.SimpleDateFormat;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+
+    private static final String INTEGER_REGEX = "0|-?([1-9]{1}[0-9]*)";
+    private static final String DOUBLE_REGEX = "-?[0-9]+\\.[0-9]+?";
+
+    private final static String EMAIL_REGEXP =
+            "^[\\.a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
+
+    private final static String LETTER_REGEXP = "^[a-zA-Z]+$";
+
+    public static boolean isCompleteLetter(String str) {
+        return !isEmpty(str) && Pattern.matches(LETTER_REGEXP, str);
+    }
+
+    //字符串不为空
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    //判断一组字符串都不为空
+    public static boolean isEmpty(String... arr) {
+        for (String str : arr) {
+            if (isEmpty(str)) return true;
+        }
+        return false;
+    }
+
+    public static int getStrLength(String str) {
+        return str == null ? -1 : str.length();
+    }
+
+    public static String trim(String text) {
+        if (text == null || "".equals(text)) {
+            return text;
+        }
+        return text.trim();
+    }
+
+    /**
+     * 判定字符串是否为int范围。
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isInt(String str) {
+        try {
+            Integer.valueOf(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * 判定字符串是否为整数。
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isInteger(String str) {
+        return !isEmpty(str) && Pattern.matches(INTEGER_REGEX, str);
+    }
+
+    /**
+     * 判断字符串是否为浮点数
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isFloat(String str) {
+        return !isEmpty(str) && Pattern.matches(DOUBLE_REGEX, str);
+    }
+
+    public static boolean isBiggerZero(String str) {
+        return isInteger(str) && Long.parseLong(str) > 0;
+    }
+
+    public static boolean isDateFormatter(String str) {
+        try {
+            new SimpleDateFormat("yyyy-MM-dd").parse(str);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isDateTimeFormatter(String str) {
+        try {
+            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(str);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isEmail(String str) {
+        return !isEmpty(str) && Pattern.matches(EMAIL_REGEXP, str);
+    }
+
+//    public static boolean isJsonFormatter(String str) {
+//        try {
+//            JSON.parse(str);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//        return true;
+//    }
+
+
+    /* 文字转拼音大写字母 */
+//    public static String converterToFirstSpell(String chines) {
+//        return Py4jUtils.converterToFirstSpell(chines);
+//    }
+
+    /**
+     * 获取指定字符在字符串中的个数
+     */
+    public static int targetStrCount(String str, String tag) {
+        int index = 0;
+        int count = 0;
+        while ((index = str.indexOf(tag)) != -1) {
+            str = str.substring(index + tag.length());
+            count++;
+        }
+        return count;
+    }
+
+
+    public static boolean checkStrLength(String str, int maxLen) {
+        return checkStrLength(str, maxLen, true);
+    }
+
+    public static boolean checkStrLength(String str, int maxLen, boolean nullAble) {
+        if (maxLen < 0) {
+            return false;
+        }
+
+        if (!nullAble) {
+            if (str == null) {
+                return false;
+            }
+        }
+
+        return str == null || (str.trim().length() <= maxLen);
+    }
+
+//    public static List<String> convBitStatuToStr(long bitNum, String[] statusStrArr) {
+//        if (ArrayUtil.isEmpty(statusStrArr) || bitNum <= 0) {
+//            return Collections.emptyList();
+//        }
+//
+//        int index = 0;
+//        long compareNum = 1;
+//        List<String> results = new ArrayList<>(statusStrArr.length);
+//
+//        while(bitNum >= compareNum && index < statusStrArr.length) {
+//            if ((bitNum & compareNum) > 0) {
+//                results.add(statusStrArr[index]);
+//            }
+//
+//            index++;
+//            compareNum <<= 1;
+//        }
+//
+//        return results;
+//    }
+
+}
---
+++ mall/src/main/java/com/moncozgc/mall/common/utils/TimeUtils.java
@@ -0,0 +1,619 @@
+package com.moncozgc.mall.common.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class TimeUtils {
+
+    /**
+     * 一周间隔时间
+     */
+    public static final long PERIOD_WEEK = 7 * 24 * 60 * 60 * 1000L;
+
+    /**
+     * 一天得间隔时间
+     */
+    public static final long PERIOD_DAY = 24 * 60 * 60 * 1000L;
+
+    /**
+     * 一小时间隔时间
+     */
+    public static final long PERIOD_HOUR = 60 * 60 * 1000L;
+
+    /**
+     * 一个月
+     */
+    public static final long PERIOD_MONTH = 30 * 24 * 60 * 60 * 1000L;
+
+    /**
+     * 添加x天
+     */
+    public static Date addDay(Date date, int num) {
+        Calendar startDT = Calendar.getInstance();
+        startDT.setTime(date);
+        startDT.add(Calendar.DAY_OF_MONTH, num);
+        return startDT.getTime();
+    }
+
+    /**
+     * 获取当天时间: 2022-12-05 16:27:09
+     */
+    public static String inDay() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
+
+    /**
+     * 减少x天
+     */
+    public static Date subtractDay(Date date, int num) {
+        Calendar startDT = Calendar.getInstance();
+        startDT.setTime(date);
+        startDT.add(Calendar.DAY_OF_MONTH, -num);
+        return startDT.getTime();
+    }
+
+    /**
+     * string -> date ,  参数:"11:00:00"  如果小于当前时间,向后加一天
+     */
+    public static Date str_Hms_2Date(String timeString) {
+        try {
+            String[] strArr = timeString.split(":");
+
+            Calendar calendar = Calendar.getInstance();
+            if (strArr.length >= 1) {
+                calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(strArr[0]));
+            } else {
+                calendar.set(Calendar.HOUR_OF_DAY, 0);
+            }
+            if (strArr.length >= 2) {
+                calendar.set(Calendar.MINUTE, Integer.parseInt(strArr[1]));
+            } else {
+                calendar.set(Calendar.MINUTE, 0);
+            }
+            if (strArr.length >= 3) {
+                calendar.set(Calendar.SECOND, Integer.parseInt(strArr[2]));
+            } else {
+                calendar.set(Calendar.SECOND, 0);
+            }
+            Date date = calendar.getTime();
+            if (date.before(new Date())) {
+                date = addDay(date, 1);
+            }
+            return date;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 例: 2017-11-11 9:50:00
+     */
+    public static Date str_yMd_Hms_2Date(String timeString) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return simpleDateFormat.parse(timeString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 例: 2017-11-11
+     */
+    public static Date str_yMd_2Date(String timeString) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return simpleDateFormat.parse(timeString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 例: 2017-11-11 9:50:00
+     */
+    public static String date_yMd_Hms_2String(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 例: 2017-11-11
+     */
+    public static String date_yMd_2String(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param fromat 指定格式格式化
+     * @Author wuchao
+     * @Description:
+     * @Params: * @param date 日期
+     * @Date 2019/12/20
+     **/
+    public static String dateFormat(String date, String oldFormat, String newFormat) {
+
+        SimpleDateFormat oldSimpleDateFormat = new SimpleDateFormat(oldFormat);
+        SimpleDateFormat newSimpleDateFormat = new SimpleDateFormat(newFormat);
+        String newDate = "";
+        try {
+            newDate = newSimpleDateFormat.format(oldSimpleDateFormat.parse(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newDate;
+    }
+
+    /**
+     * 例: 1111 标识xx月xx日
+     */
+    public static String date_Md_2String(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMdd");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 例: 1111 标识xx月xx日
+     */
+    public static int getCurrentDate_Md() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMdd");
+        try {
+            return Integer.parseInt(simpleDateFormat.format(new Date()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    /**
+     * 例: 2017-11-11 9:50:00
+     */
+    public static String date_Hms_2String(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 毫秒数-> x天x小时x分x秒
+     *
+     * @author lzp
+     */
+    public static String formatDuring(long mss) {
+        long days = mss / (1000 * 60 * 60 * 24);
+        long hours = (mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+        long minutes = (mss % (1000 * 60 * 60)) / (1000 * 60);
+        long seconds = (mss % (1000 * 60)) / 1000;
+        StringBuilder sb = new StringBuilder();
+        if (days > 0) {
+            sb.append(days + "天");
+        }
+        if (hours > 0) {
+            sb.append(hours + "小时");
+        }
+        if (minutes > 0) {
+            sb.append(minutes + "分钟");
+        }
+        if (seconds > 0) {
+            sb.append(seconds + "秒");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取当前年份
+     */
+    public static int getCurrentYear() {
+        try {
+            return Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 1900;
+    }
+
+    public static int[] getCurrAndLastYear() {
+        int curr = getCurrentYear();
+
+        return new int[]{curr - 1, curr};
+    }
+
+    public static int[] getDBableCurrAndLastYear() {
+        int curr = getCurrentYear();
+
+        if (curr < 2019) {
+            return new int[0];
+        }
+
+        if (curr == 2019) {
+            return new int[]{curr};
+        }
+
+        return new int[]{curr - 1, curr};
+    }
+
+    /**
+     * 获取指定年份
+     */
+    public static int getCurrentYear(Date date) {
+        try {
+            return Integer.parseInt(new SimpleDateFormat("yyyy").format(date));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 1900;
+    }
+
+    /**
+     * 获取当前日期
+     */
+    public static String getCurrentDate() {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return "1900-01-01";
+    }
+
+    public static String getDate() {
+        try {
+            return new SimpleDateFormat("yyyy年MM月dd日").format(new Date());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return "1900年01月01日";
+    }
+
+    public static String getDate(String date) {
+        try {
+            return new SimpleDateFormat("yyyy年mm月dd日").format(date);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return "1900年01月01日";
+    }
+
+    /**
+     * 获取当前时间
+     */
+    public static String getCurrentTime() {
+        try {
+            return new SimpleDateFormat("HH:mm:ss").format(new Date());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return "00:00:00";
+    }
+
+
+    /**
+     * 格式为"HH:mm:ss"
+     * 判断当前时间是否在[startDate, endDate]区间，注意时间格式要一致
+     *
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public static boolean isEffectiveTime(String startTime, String endTime) {
+        try {
+            String format = "HH:mm:ss";
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            String nowTime = sdf.format(new Date());
+            Date nowDate = sdf.parse(nowTime);
+            Date startDate = sdf.parse(startTime);
+            Date endDate = sdf.parse(endTime);
+            return isEffectiveDate(nowDate, startDate, endDate);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 判断当前时间是否在[startDate, endDate]区间，注意时间格式要一致
+     *
+     * @param nowDate   当前时间
+     * @param startDate 开始时间
+     * @param endDate   结束时间
+     * @return
+     */
+    public static boolean isEffectiveDate(Date nowDate, Date startDate, Date endDate) {
+        if (nowDate.getTime() == startDate.getTime()
+                || nowDate.getTime() == endDate.getTime()) {
+            return true;
+        }
+
+        Calendar date = Calendar.getInstance();
+        date.setTime(nowDate);
+
+        Calendar begin = Calendar.getInstance();
+        begin.setTime(startDate);
+
+        Calendar end = Calendar.getInstance();
+        end.setTime(endDate);
+
+        if (date.after(begin) && date.before(end)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * 判断时间1是否在时间2之后
+     *
+     * @param date1 时间1
+     * @param date2 时间2
+     * @return
+     */
+    public static boolean after(String date1, String date2) {
+
+        Date d1 = str_yMd_Hms_2Date(date1);
+        Date d2 = str_yMd_Hms_2Date(date2);
+
+        return d1.after(d2);
+    }
+
+    public static boolean afterDay(String date1, String date2) {
+
+        Date d1 = str_yMd_2Date(date1);
+        Date d2 = str_yMd_2Date(date2);
+
+        return d1.after(d2);
+    }
+
+    public static int getYearByOrderno(String orderno) {
+        if (StringUtils.isEmpty(orderno)) {
+            return getCurrentYear();
+        }
+
+        try {
+            return Integer.parseInt("20" + orderno.substring(0, 2));
+        } catch (Exception e) {
+            return 0;
+        }
+
+    }
+
+    /**
+     * @Author wuchao
+     * @Description:字符串转日期
+     * @Params: * @param str
+     * @Date 2019/10/10
+     **/
+    public static Date convertStringToDate(String str) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = null;
+        try {
+            date = sdf.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    /**
+     * @Author wuchao
+     * @Description:获取当前日期
+     * @Params: * @param
+     * @Date 2019/11/18
+     **/
+    public static String getNewDate() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(calendar.DATE, 0);//如果把0修改为-1就代表昨天
+        date = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = format.format(date);
+        return dateString;
+    }
+
+    /**
+     * @Author wuchao
+     * @Description:获取当前日期
+     * @Params: * @param
+     * @Date 2019/11/18
+     **/
+    public static String date2_yy_MM_dd_HH_mm_ss() {
+        SimpleDateFormat format = new SimpleDateFormat("yyMMddHHmmss");
+        String dateString = format.format(new Date());
+        return dateString;
+    }
+
+    /**
+     * @Author wuchao
+     * @Description:获取当前日期
+     * @Params: * @param
+     * @Date 2019/11/21
+     **/
+    public static String getNewToDate() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(calendar.DATE, 0);//如果把0修改为-1就代表昨天
+        date = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(date);
+        return dateString;
+    }
+
+    /**
+     * @Author wuchao
+     * @Description:获取当前日期
+     * @Params: * @param
+     * @Date 2019/11/21
+     **/
+    public static String getSpecNumberDateToString(int dayNumber) {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(calendar.DATE, dayNumber);//如果把0修改为-1就代表昨天
+        date = calendar.getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(date);
+        return dateString;
+    }
+
+
+    /**
+     * 获取两个时间相差多少天
+     *
+     * @param sDate
+     * @param eDate
+     * @return
+     */
+    public static long getDateDiscrepancyDayNumber(String sDate, String eDate) {
+        Date sdate = str_yMd_2Date(sDate);//开始日期
+        Date edate = str_yMd_2Date(eDate);//结束日期
+
+        Calendar scalendar = Calendar.getInstance();
+        scalendar.setTime(sdate);
+        long sDay = scalendar.getTimeInMillis();
+
+
+        Calendar ecalendar = Calendar.getInstance();
+        ecalendar.setTime(edate);
+        long eDay = ecalendar.getTimeInMillis();
+
+        long disTimes = eDay - sDay;
+        if (disTimes <= 0) {
+            return 0;
+        }
+        return (disTimes / 1000 / 60 / 60 / 24);
+    }
+
+    /**
+     * 获取两个时间相差多少天
+     *
+     * @param sDate
+     * @param eDate
+     * @return
+     */
+    public static int getMonthDiff(String sDate, String eDate) {
+        Date sdate = str_yMd_2Date(sDate);
+        Date edate = str_yMd_2Date(eDate);
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+        c1.setTime(sdate);
+        c2.setTime(edate);
+        int year1 = c1.get(Calendar.YEAR);
+        int year2 = c2.get(Calendar.YEAR);
+        int month1 = c1.get(Calendar.MONTH);
+        int month2 = c2.get(Calendar.MONTH);
+        int day1 = c1.get(Calendar.DAY_OF_MONTH);
+        int day2 = c2.get(Calendar.DAY_OF_MONTH);
+        // 获取年的差值 
+        int yearInterval = year1 - year2;
+        // 如果 d1的 月-日 小于 d2的 月-日 那么 yearInterval-- 这样就得到了相差的年数
+        if (month1 < month2 || month1 == month2 && day1 < day2) {
+            yearInterval--;
+        }
+        // 获取月数差值
+        int monthInterval = (month1 + 12) - month2;
+        if (day1 < day2) {
+            monthInterval--;
+        }
+        monthInterval %= 12;
+        int monthsDiff = Math.abs(yearInterval * 12 + monthInterval);
+        return monthsDiff;
+    }
+
+    /**
+     * 例: 2017-11
+     */
+    public static String date_yM_2String(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取当前时间几年前或是几年后
+     *
+     * @param distance 距离年数
+     * @param addOrSub 年前年后 true-年前 false 年后
+     * @return
+     */
+    public static String getYearDistanceByValue(int distance, boolean addOrSub) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        if (addOrSub) {
+            year = year - distance;
+        } else {
+            year = year + distance;
+        }
+
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(Calendar.YEAR, year);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(calendar1.getTime());
+        return dateString;
+    }
+
+    private static final String TIME_MATCH_REGULAR_YYYYMMDD_HHMMSS = "(\\d{1,4})(-|\\/)(\\d{1,2})\\2(\\d{1,2})\\s+(\\d{1,2}):(\\d{1,2}):(\\d{1,2})";
+    private static final String TIME_MATCH_REGULAR_YYYYMMDD = "(\\d{1,4})(-|\\/)(\\d{1,2})\\2(\\d{1,2})";
+
+    public static String[] regularMatchTimeStr(String text) {
+        List<String> list = new ArrayList<>();
+
+        Pattern r1 = Pattern.compile(TIME_MATCH_REGULAR_YYYYMMDD_HHMMSS);
+        Matcher m1 = r1.matcher(text);
+        while (m1.find()) {
+            String tstr = m1.group();
+            list.add(tstr);
+            text = text.replaceFirst(tstr, "");
+        }
+
+        Pattern r2 = Pattern.compile(TIME_MATCH_REGULAR_YYYYMMDD);
+        Matcher m2 = r2.matcher(text);
+        while (m2.find()) {
+            String tstr = m2.group();
+            list.add(tstr);
+            text = text.replaceFirst(tstr, "");
+        }
+
+
+        String[] arr = new String[list.size()];
+        return list.toArray(arr);
+    }
+
+}
