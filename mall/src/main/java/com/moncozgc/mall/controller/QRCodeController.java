3f47a0e8f9a9618c2a629453f4c69a41654744af
MoncozGC <jadepengc@163.com>
2022-12-06 15:58:11 +0800

    style: 添加注释

--- mall/src/main/java/com/moncozgc/mall/controller/QRCodeController.java
+++ mall/src/main/java/com/moncozgc/mall/controller/QRCodeController.java
@@ -3,7 +3,9 @@ package com.moncozgc.mall.controller;
 import com.moncozgc.mall.common.api.CommonResult;
 import com.moncozgc.mall.common.utils.QRCodeUtil;
 import com.moncozgc.mall.dto.Base64Dto;
-import lombok.extern.slf4j.Slf4j;
+import com.moncozgc.mall.service.impl.TaskServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletOutputStream;
@@ -11,17 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * <p>
  * QRCodeController
- * </p>
+ * https://zhuanlan.zhihu.com/p/158576491
  *
  * @author rcbb.cc
  * @date 2022/8/28
  */
-@Slf4j
 @RestController
 @RequestMapping("/qrcode")
 public class QRCodeController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(TaskServiceImpl.class);
 
     /**
      * 根据 content 生成二维码
@@ -41,7 +43,7 @@ public class QRCodeController {
         if (width != null && height != null) {
             return CommonResult.success(QRCodeUtil.getBase64QRCode(content, width, height, logoUrl, 100, 100), "二维码转换成功(添加LOG)");
         }
-        log.info(">>> 二维码生成成功");
+        LOGGER.info(">>> 二维码base64生成成功");
         return CommonResult.success(QRCodeUtil.getBase64QRCode(content, logoUrl), "二维码转换成功");
     }
 
@@ -60,7 +62,9 @@ public class QRCodeController {
         try {
             stream = response.getOutputStream();
             QRCodeUtil.getQRCode(content, logoUrl, stream);
+            LOGGER.info(">>> 二维码图片生成成功");
         } catch (Exception e) {
+            LOGGER.error(">>> 二维码图片生成失败");
             e.getStackTrace();
         } finally {
             if (stream != null) {
@@ -78,8 +82,8 @@ public class QRCodeController {
     @RequestMapping(value = "/QRCodeBase64ToPic", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
     public CommonResult<Object> Base64ToImg2(@RequestBody Base64Dto base64Dto) throws IOException {
-        QRCodeUtil.GenerateImage(base64Dto, System.getProperty("user.dir") + "\\mall\\src\\main\\data\\");
-        log.info(">>> base64Topic完成");
+        QRCodeUtil.GenerateImage(base64Dto, System.getProperty("user.dir") + "\\");
+        LOGGER.info(">>> base64Topic完成");
         return CommonResult.success("转换成功");
     }
 
