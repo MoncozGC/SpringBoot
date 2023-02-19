82636c8c9f4d04c87a2ad223f3c23baeaa2c5fcb
MoncozGC <jadepengc@163.com>
2023-02-19 21:45:10 +0800

    feat: mall - Application, 添加注解 @EnableDiscoveryClient将微服务注册到服务组件种, 让注册中心能够发现，扫描到该服务 @EnableFeignClients, 配合@FeignClient注解使用, 调用其他服务接口

--- mall/src/main/java/com/moncozgc/mall/MallApplication.java
+++ mall/src/main/java/com/moncozgc/mall/MallApplication.java
@@ -3,6 +3,8 @@ package com.moncozgc.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +13,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @EnableScheduling // 定时任务执行功能注解
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableDiscoveryClient // 将微服务注册到服务组件种, 让注册中心能够发现，扫描到该服务
+@EnableFeignClients // 配合@FeignClient注解使用, 调用其他服务接口
+//@EnableGlobalMethodSecurity(prePostEnabled = true) // 判断用户对某个控制层的方法是否具有控制权限
 @MapperScan(basePackages = "com.moncozgc.mall.mapper")
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
